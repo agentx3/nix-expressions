13c65786a37fb9ed3301076d77fceb2dbcb90fa4
agentx3 <thepuzzlegang.agent@gmail.com>
2025-01-06 21:30:20 -0800

    Formatting

--- packages/default.nix
+++ packages/default.nix
@@ -3,9 +3,8 @@ let
   inherit (pkgs) callPackage;
 in
 {
-
   frankendrift = callPackage ./frankendrift { };
-  pycln = pkgs.python310Packages.callPackage ./pycln { };
+  pycln = pkgs.python310Packages.callPackage ./pythonPackages/pycln { };
   gocv = callPackage ./goPackages/gocv { };
   fblitz = callPackage ./goPackages/fblitz { };
   keydogger = callPackage ./keydogger { };
